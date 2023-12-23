cad0615385045d353e8ec3f67f6639258d00a89d
Phat Nguyen <hihoannn.nii@gmail.com>
2023-12-23 13:06:55 +0700

    Add common class

--- src/app/_metronic/layout/components/sidebar/sidebar-menu/sidebar-menu.component.ts
+++ src/app/_metronic/layout/components/sidebar/sidebar-menu/sidebar-menu.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/_fake/services/user-service';
+import { AuthService } from 'src/app/modules/auth';
 
 @Component({
-  selector: 'app-sidebar-menu',
-  templateUrl: './sidebar-menu.component.html',
-  styleUrls: ['./sidebar-menu.component.scss']
+    selector: 'app-sidebar-menu',
+    templateUrl: './sidebar-menu.component.html',
+    styleUrls: ['./sidebar-menu.component.scss']
 })
 export class SidebarMenuComponent implements OnInit {
 
-  constructor() { }
+    constructor(private user: AuthService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        console.log(this.user.currentUserValue)
+    }
 
 }
