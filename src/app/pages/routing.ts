ad78aa407f5294c3e040852813ea67ef99774a1d
Phat Nguyen <hihoannn.nii@gmail.com>
2023-12-23 16:36:22 +0700

    Merge branch 'main' of https://github.com/hyphens-hyphens/PPLTHD_FE
    # Conflicts:
    #	src/app/app.module.ts
    #	src/app/features/ads-location/ads-location-form/ads-location-form.component.ts
    #	src/app/features/ads-location/ads-location.component.ts
    #	src/app/pages/routing.ts

--- src/app/pages/routing.ts
+++ src/app/pages/routing.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { AdsLocationComponent } from '../features/ads/ads.component';
-=======
 import { AdsLocationComponent } from '../features/ads-location/ads-location.component';
->>>>>>> c5788e1a776aca8407c3e98b08c665c28c81bac6
 
 const Routing: Routes = [
 
